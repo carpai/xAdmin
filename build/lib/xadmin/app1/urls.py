989f51154d15d90e9f2e6d73156d733a27489f2c
Binghoo Dang <dangbinghoo@gmail.com>
2015-05-27 11:04:38 +0000

    clean project, we just need project files

--- build/lib/xadmin/app1/urls.py
+++
@@ -1,8 +0,0 @@
-
-# app1 url
-urlpatterns = [
-    [r'getusr', r'/getusr'],
-    [r'addusr', r'/adduser'],
-    [r'addusraction', r'/addusraction']
-]
-
--- build/lib/xadmin/app2/urls.py
+++
@@ -1,7 +0,0 @@
-
-# app2 url dispatch
-urlpatterns = [
-    ['home', '/'],
-    ['say', '/saytxt'],
-    ['dict', '/dict']
-]

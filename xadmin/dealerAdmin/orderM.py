7aed5ca6fd1dad195dfa9a1cf0351004b3b9d1d7
Binghoo Dang <dangbinghoo@gmail.com>
2015-08-25 08:55:06 +0000

    增加几个页面

--- xadmin/dealerAdmin/orderM.py
+++ xadmin/dealerAdmin/orderM.py
@@ -1,1 +1,23 @@
-__author__ = 'playpeng'
+#!/usr/bin/env python3
+
+from mongokit import Document
+
+class Order(Document):
+    structure = {
+        'id_str'          : str,
+        'user_id'         : str,
+        'dealer_id'       : str,
+        'good_id'         : list,
+        'unit'            : str,
+        'counts'          : str,
+        'price'           : str,
+        'payment'         : str,
+        'create_time'     : str,
+        'endtime'         : str,
+
+        'status'          : int
+    }
+
+    use_dot_notation = True
+
+
